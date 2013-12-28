33eadc4afba51854fd6b0d5c9d844c97bd307200
Giuseppe Rota <rota.giuseppe@gmail.com>
2013-12-28 18:37:02 +0000

    depend on artifact cookbook (purposedly failing)

--- site-cookbooks/myface/metadata.rb
+++ site-cookbooks/myface/metadata.rb
@@ -1,8 +1,8 @@
-name             'site-cookbooks/myface'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
+name             'myface'
+maintainer       'Giuseppe Rota'
+maintainer_email 'rota.giuseppe@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures site-cookbooks/myface'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-
+depends 'artifact', '~> 0.10.7'
--- site-cookbooks/myface/recipes/default.rb
+++ site-cookbooks/myface/recipes/default.rb
@@ -9,3 +9,12 @@
 log "WELCOME TO CHEF MOFO" do
   level :info
 end
+
+artifact_deploy "myface" do
+  version "1.0.0"
+  artifact_location "http://24.media.tumblr.com/tumblr_m7fpxfkHM81rzupqxo1_500.png"
+  deploy_to "/srv/myface"
+  owner "myface"
+  group "myface"
+  action :deploy
+end
