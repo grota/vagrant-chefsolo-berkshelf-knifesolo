9de11ab644ea1ba77b2692ee8a09284956e03764
Giuseppe Rota <rota.giuseppe@gmail.com>
2013-12-29 16:11:30 +0000

    create database and db user

--- site-cookbooks/myface/metadata.rb
+++ site-cookbooks/myface/metadata.rb
@@ -7,4 +7,5 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends 'artifact', '~> 0.10.7'
 depends "tomcat", "~> 0.11.0"
-depends "mysql", "~> 1.3.0"
+#depends "mysql", "~> 1.3.0"
+depends "database", "~> 1.3.4"
--- site-cookbooks/myface/recipes/database.rb
+++ site-cookbooks/myface/recipes/database.rb
@@ -1,1 +1,28 @@
 include_recipe "mysql::server"
+
+service "mysql" do
+  action :enable
+end
+
+include_recipe "database::mysql"
+
+mysql_database "myface_dev" do
+  connection(
+    :host => "localhost",
+    :username => "root",
+    :password => node[:mysql][:server_root_password]
+  )
+  action :create
+end
+
+mysql_database_user "myface_app" do
+  connection(
+    :host => "localhost",
+    :username => "root",
+    :password => node[:mysql][:server_root_password]
+  )
+  password "supersecret"
+  database_name "myface_dev"
+  host "localhost"
+  action [:create, :grant]
+end
